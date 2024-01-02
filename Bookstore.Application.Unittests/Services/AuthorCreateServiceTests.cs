ffa3da10a60d0241762fe263853f21bb8a5c7c78
Mohamad majed Frejha <majedfrejha@gmail.com>
2024-01-02 17:20:36 +0100

    Projektdateien hinzufügen.

---
+++ Bookstore.Application.Unittests/Services/AuthorCreateServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Services;
+using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bookstore.Application.Unittests.Services
+{
+    public class AuthorCreateServiceTests
+    {
+        private IMapper Mapper { get; }
+        private AuthorCreateValidator Validator { get; }
+
+        public AuthorCreateServiceTests()
+        {
+            Mapper = new MapperConfiguration(cfg =>
+            cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
+            Validator = new AuthorCreateValidator();
+        }
+
+        [Fact]
+        public async Task Author_Created()
+        {
+            //Arrange
+            var authorCreate = new AuthorCreate("Test", "Test");
+            var authorRepositoryMock = new Mock<IAuthorRepository>();
+            var authorCreateSerice = new AuthorCreateService(authorRepositoryMock.Object,
+                Mapper, Validator);
+
+            //Act
+            await authorCreateSerice.CreateAuthorAsync(authorCreate);
+
+            //Assert
+            authorRepositoryMock.Verify(authorRepositoryMock =>
+            authorRepositoryMock.AddAuthorAsync(It.IsAny<Author>()), Times.Once);
+        }
+    }
+}
---
+++ Bookstore.Application/Services/AuthorCreateService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
+using FluentValidation;
+
+namespace Bookstore.Application.Services;
+
+public class AuthorCreateService
+{
+    public IAuthorRepository AuthorRepository { get; }
+    public IMapper Mapper { get; }
+    public AuthorCreateValidator AuthorCreateValidator { get; }
+
+    public AuthorCreateService(IAuthorRepository authorRepository, IMapper mapper,
+        AuthorCreateValidator authorCreateValidator)
+    {
+        AuthorRepository = authorRepository;
+        Mapper = mapper;
+        AuthorCreateValidator = authorCreateValidator;
+    }
+
+    public async Task<long> CreateAuthorAsync(AuthorCreate authorCreate)
+    {
+        await AuthorCreateValidator.ValidateAndThrowAsync(authorCreate);
+        var author = Mapper.Map<Author>(authorCreate);
+        long id = await AuthorRepository.AddAuthorAsync(author);
+        return id;
+    }
+
+}
